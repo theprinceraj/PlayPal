0f32958899835f7542088464303051e122269255
Bhaalu-69 <discordsimpcity+github@gmail.com>
2023-07-16 12:22:49 +0530

    .

--- commands/buba.js
+++ commands/buba.js
@@ -1,26 +1,27 @@
 const { EmbedBuilder } = require('discord.js');
 
-exports.run = (client, message, args) =>  {
+exports.run = (client, message, args) => {
     try {
         const user = message.mentions.users.first() || message.author;
-  const bubaComments = [
-    "${user} has so small booba that its not even visible through microscope.",
-    "${user} is a no booba person.",
-    "${user} has decent sized booba.",
-    "${user} has so large boobas that they cannot walk, OMG!",
-    "${user} has as large booba as Daksh's ass.",
-    "${user} had no boobaa initially but they went through gender transplant and now have developed some!"
-  ];
-  const response = bubaComments[Math.floor(Math.random() * bubaComments.length)].replace("${user}", user);
+        const bubaComments = [
+            "${user} has so small booba that its not even visible through microscope.",
+            "${user} is a no booba person.",
+            "${user} has decent sized booba.",
+            "${user} has so large boobas that they cannot walk, OMG!",
+            "${user} has as large booba as Daksh's ass.",
+            "${user} had no boobaa initially but they went through gender transplant and now have developed some!"
+        ];
+        const response = bubaComments[Math.floor(Math.random() * bubaComments.length)].replace("${user}", user);
         const embed = new EmbedBuilder()
-        .setTitle(`${client.user.username}'s Buba Predictor`)
-        .setDescription(`${response}`)
-        .setTimestamp()
-        .setFooter({text:message.guild.name, iconURL:message.guild.iconURL()})
-    } catch(e) {
+            .setTitle(`${client.user.username}'s Buba Predictor`)
+            .setDescription(`${response}`)
+            .setTimestamp()
+            .setFooter({ text: message.guild.name, iconURL: message.guild.iconURL() })
+        message.reply({ embeds: [embed] });
+    } catch (e) {
         console.error(`Error encountered in buba.js:\n${e}`);
     }
-    message.reply({embeds: [embed]});
+
 }
 
 exports.name = "buba";
