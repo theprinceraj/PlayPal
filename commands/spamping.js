911a58256c508adea8db718a0f5b1dbae9512b78
theprinceraj <discordsimpcity+github@gmail.com>
2023-07-25 13:07:03 +0530

    fixed spamping.js

--- commands/spamping.js
+++ commands/spamping.js
@@ -1,11 +1,8 @@
 exports.run = (client, message, args) => {
-  if (!member) return;
-  if (message.author.id === '564327207133249536') {
-    const user = message.mentions.users.first();
-    if (!user) return;
+  const user = message.mentions.users.first();
+  if (!user && message.author.id !== '564327207133249536') return;
     for (let i = 0; i < 10; i++) {
       message.channel.send(`${user}`);
-    }
   }
 };
 
