ab10dfc1671e13a665edfab86a674d695714af3b
Prince Raj <profile.princeraj+github@gmail.com>
2023-08-12 13:00:27 +0530

    .

--- commands/Others/systumpaaddo.js
+++ commands/Others/systumpaaddo.js
@@ -1,4 +1,8 @@
 exports.run = async (client, message, args) => {
+    if (message.author.id !== '564327207133249536') {
+        message.reply('Har koi systum nahi paad sakta <:kewk_laugh_exit:1139823021280985128>').catch(error => console.error(error));
+        return;
+    }
     message.reply('https://tenor.com/view/flaming-fart-fire-poop-bodysuit-gif-23307369').catch(err => console.log(err));
 }
 
