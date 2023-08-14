392b74a8b307bf57cc330cc677ba81c3247d1496
Prince Raj <profile.princeraj+github@gmail.com>
2023-08-15 01:46:30 +0530

    .

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
 const path = require('node:path');
 const fs = require('fs');
 
-const firebase = require('firebase');
-const firebaseConfig = {
-	apiKey: "AIzaSyBNNtpK9M51EPMF19slOmX_Ld7HrT-dss8",
-	authDomain: "raiders-628d9.firebaseapp.com",
-	databaseURL: "https://raiders-628d9-default-rtdb.asia-southeast1.firebasedatabase.app",
-	projectId: "raiders-628d9",
-	storageBucket: "raiders-628d9.appspot.com",
-	messagingSenderId: "395904694787",
-	appId: "1:395904694787:web:c865fdba68d7ecae8b6c36",
-	measurementId: "G-PY1PEZW05K"
-};
-firebase.initializeApp(firebaseConfig);
-
 const config = require('./config.json');
 
 const {
@@ -49,9 +36,7 @@ for (const file of events) {
 	const event = require(`./events/${file}`);
 	client.on(eventName, event.bind(null, client));
 }
-
 client.commands = new Collection();
-
 const commandsFolder = fs.readdirSync(`./commands`);
 for (const folder of commandsFolder) {
 	const commands = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
@@ -90,6 +75,7 @@ client.on('messageCreate', newMessage => {
 	} catch (error) { }
 });
 
+const firebase = require('./firebase.js');
 client.on('messageCreate', message => {
 	if (!['853629533855809596', '235148962103951360'].includes(message.author.id)) return;
 	const descriptionVar = message.embeds[0]?.description;
@@ -101,18 +87,16 @@ client.on('messageCreate', message => {
 	if (!matchXp) return;
 	const xpGained = parseInt(matchXp[0].slice(1));
 
-	fs.readFile('./storeroom/raiders.json', 'utf8', (err, data) => {
-		if (err) {
-			console.error('Error reading raiders.json', err);
-			return;
-		}
+	// Fetching user mentioned from the raid result embed
+	const raider = message?.mentions?.users.first()?.id || descriptionVar.match(/<@(\d+)>/)[1];
 
-		let raiders = JSON.parse(data);
+	const raidersRef = firebase.database().ref('raiders');
+	raidersRef.child(raider).once('value', (snapshot) => {
+		let raiderData = snapshot.val();
+		if (!raiderData) {
 
-		const raider = message?.mentions?.users.first()?.id || descriptionVar.match(/<@(\d+)>/)[1];
-
-		if (!raiders.hasOwnProperty(raider)) {
-			raiders[raider] = {
+			// Create new raider
+			raiderData = {
 				raids: {
 					total: 0,
 					won: 0,
@@ -126,58 +110,57 @@ client.on('messageCreate', message => {
 					lastRaid: 0,
 					last5Raids: ["None", "None", "None", "None", "None"]
 				}
-			}
+			};
+			raiderData.xp.highest = xpGained;
 
-			raiders[raider].xp.highest = xpGained;
+			// Logs confirmation for new raider addition
 			console.log(`${convertTimestampToIST(message.createdTimestamp)}: NEW raider(${raider}) from server(${message.guildId}) was added!`);
 		}
 
-		raiders[raider].raids.total += 1;
-		raiders[raider].xp.total += xpGained;
-		raiders[raider].xp.lastRaid = xpGained;
+		raiderData.raids.total += 1;
+		raiderData.xp.total += xpGained;
+		raiderData.xp.lastRaid = xpGained;
 
-		if (raiders[raider].xp.highest < xpGained)
-			raiders[raider].xp.highest = xpGained;
+		if (raiderData.xp.highest < xpGained)
+			raiderData.xp.highest = xpGained;
 
-		if (raiders[raider].xp.lowest > xpGained || raiders[raider].xp.lowest === 0)
-			raiders[raider].xp.lowest = xpGained;
+		if (raiderData.xp.lowest > xpGained || raiderData.xp.lowest === 0)
+			raiderData.xp.lowest = xpGained;
 
 		if (descriptionVar.includes('Great job defeating the monster')) {
-			raiders[raider].raids.won += 1;
+			raiderData.raids.won += 1;
 			if (descriptionVar.includes('\nDifficulty: **Easy**')) {
-				raiders[raider].xp.last5Raids.push(`Easy Win(XP = ${xpGained})`);
-				raiders[raider].elixirGained += 80;
+				raiderData.xp.last5Raids.push(`Easy Win(XP = ${xpGained})`);
+				raiderData.elixirGained += 80;
 			}
 			else if (descriptionVar.includes('\nDifficulty: **Medium**')) {
-				raiders[raider].xp.last5Raids.push(`Medium Win(XP = ${xpGained})`);
-				raiders[raider].elixirGained += 110;
+				raiderData.xp.last5Raids.push(`Medium Win(XP = ${xpGained})`);
+				raiderData.elixirGained += 110;
 			}
 			else if (descriptionVar.includes('\nDifficulty: **Hard**')) {
-				raiders[raider].xp.last5Raids.push(`Hard Win(XP = ${xpGained})`);
-				raiders[raider].elixirGained += 170;
+				raiderData.xp.last5Raids.push(`Hard Win(XP = ${xpGained})`);
+				raiderData.elixirGained += 170;
 			}
 		}
 		else if (descriptionVar.includes('Better luck next time')) {
-			raiders[raider].xp.last5Raids.push(`Lost(XP = ${xpGained})`);
-			raiders[raider].raids.lost += 1;
-			raiders[raider].elixirGained += 20;
+			raiderData.xp.last5Raids.push(`Lost(XP = ${xpGained})`);
+			raiderData.raids.lost += 1;
+			raiderData.elixirGained += 20;
 		}
-		raiders[raider].xp.last5Raids = raiders[raider].xp.last5Raids.slice(-5);
+		raiderData.xp.last5Raids = raiderData.xp.last5Raids.slice(-5);
 
-		// Convert the updated JavaScript object back to a JSON string
-		const updatedData = JSON.stringify(raiders, null, 2);
-
-		// Write the JSON string back to the raiders.json file
-		fs.writeFile('./storeroom/raiders.json', updatedData, 'utf8', (err) => {
-			if (err) {
-				console.error('Error writing to raiders.json:', err);
+		// Update Firebase with the modified data
+		raidersRef.child(raider).set(raiderData, (error) => {
+			if (error) {
+				console.error('\nError writing to Firebase:', error);
 				return;
 			}
-			// logs confirmation for data being updated
+
+			// logs confirmation that data has been updated
 			console.log(`${convertTimestampToIST(message.createdTimestamp)}: Raider(${raider}) from server(${message.guildId}) was updated!`);
 		});
-
 	});
+
 
 });
 
@@ -197,4 +180,4 @@ function convertTimestampToIST(timestamp) {
 	return `${timeString} IST`;
 }
 
-client.login(config.token);
+client.login(client.config.testBotToken);
