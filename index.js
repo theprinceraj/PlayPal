bfd114d7190ca093e895483e4202434b1bb2929a
Prince Raj <profile.princeraj+github@gmail.com>
2023-08-15 20:58:09 +0530

    .

--- index.js
+++ index.js
@@ -171,7 +171,7 @@ client.on('messageCreate', newMessage => {
 // Using firebase
 const firebase = require('./firebase.js');
 client.on('messageCreate', message => {
-	if (message.author.id === '853629533855809596') return;
+	if (message.author.id !== '853629533855809596') return;
 	// if (!['853629533855809596', '235148962103951360'].includes(message.author.id)) return;
 	const descriptionVar = message.embeds[0]?.description;
 
