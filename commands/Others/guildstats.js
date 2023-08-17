6328d45cc9ceebeaa1f331b308e2ebd3103a5a85
Prince Raj <profile.princeraj+github@gmail.com>
2023-08-17 23:31:27 +0530

    Added a select menu to the guildstats command that allows users to sort the raiders by ascending or descending order of their XP.

--- commands/Others/guildstats.js
+++ commands/Others/guildstats.js
@@ -1,5 +1,16 @@
-const { EmbedBuilder } = require('discord.js');
+const { EmbedBuilder, StringSelectMenuOptionBuilder, StringSelectMenuBuilder, ActionRowBuilder } = require('discord.js');
 const firebase = require('../../firebase.js');
+
+function convertArrayToString(arr) {
+    let result = '';
+
+    for (let i = 0; i < arr.length; i++) {
+        const raiderKey = arr[i].raiderId;
+        result += `${i + 1}. \`${raiderKey}\`\n`;
+    }
+
+    return result.trim();
+}
 
 exports.run = (client, message, args) => {
     try {
@@ -26,7 +37,7 @@ exports.run = (client, message, args) => {
         }
 
         const raidersRef = firebase.database().ref('raiders');
-        raidersRef.child(raiderSearched.id).once('value', (snapshot) => {
+        raidersRef.child(raiderSearched.id).once('value', async (snapshot) => {
             const raiderData = snapshot.val();
             if (!raiderData) {
                 message.reply('No stat found, do a raid with `sgr` and then try again.').catch(err => console.log(err));
@@ -34,7 +45,7 @@ exports.run = (client, message, args) => {
             }
 
             // Make embed for displaying stats
-            const statsDisplayEmbed = new EmbedBuilder()
+            let statsDisplayEmbed = new EmbedBuilder()
                 .setTitle(`**Raider Stats for \`${raiderSearched.username}\`**`)
                 .setThumbnail(raiderSearched.displayAvatarURL())
                 .setColor('#6666ff')
@@ -47,8 +58,59 @@ exports.run = (client, message, args) => {
                     { name: '<a:rocket_emoji:1141006841132879872> Add Me', value: `[Click here](https://discord.com/api/oauth2/authorize?client_id=1104277580577783848&permissions=395137371201&scope=bot)` }
                 )
 
-            // message.reply({ content: 'Grace period for the guild season ends <t:1692210540:R>. The raid stats for everyone(in PlayPal) will be reset <t:1692206940:R>. Message @bhaalu if you have any queries.', embeds: [statsDisplayEmbed] }).catch(err => console.log(err))
-            message.reply({ embeds: [statsDisplayEmbed] }).catch(err => console.log(err))
+            if (message.author.id === '564327207133249536') {
+                // Make a select menu
+                const select = new StringSelectMenuBuilder()
+                    .setCustomId('guild-stats-dropdown')
+                    .setPlaceholder('Make a selection!')
+                    .addOptions(
+                        new StringSelectMenuOptionBuilder()
+                            .setLabel('Ascending Order')
+                            .setDescription('Sorts people in ascending order of their XP.')
+                            .setValue('ascending')
+                            .setEmoji('↗️'),
+                        new StringSelectMenuOptionBuilder()
+                            .setLabel('Descending Order')
+                            .setDescription('Sorts people in ascending order of their XP.')
+                            .setValue('descending')
+                            .setEmoji('↘️'),
+                    );
+                const actionRow = new ActionRowBuilder()
+                    .addComponents(select)
+
+                let initialMessage = await message.reply({ embeds: [statsDisplayEmbed], components: [actionRow] }).catch(err => console.log(err))
+
+                client.on('interactionCreate', async (interaction) => {
+                    if (!interaction.isStringSelectMenu() || interaction.customId !== 'guild-stats-dropdown') return;
+                    let arrayOfRaiders = [];
+                    const raidersDatabase = await raidersRef.once('value')
+                    raidersDatabase.forEach(raiderEntry => {
+                        let raiderId = raiderEntry.key;
+                        let raiderDetails = raiderEntry.val();
+                        arrayOfRaiders.push({ raiderId: raiderId, totalXp: raiderDetails.xp.total })
+                    })
+                    let sortedArray = [];
+                    if (interaction.values[0] === 'ascending') {
+                        sortedArray = arrayOfRaiders.sort((a, b) => a.totalXp - b.totalXp)
+                    } else if (interaction.values[0] === 'descending') {
+                        sortedArray = arrayOfRaiders.sort((a, b) => b.totalXp - a.totalXp)
+                    }
+
+                    let trimmedArrayOfRaiders = sortedArray.slice(0,6);
+
+                    // Remove fields from the embed
+                    [0, 1, 2].forEach(index => {
+                        statsDisplayEmbed.spliceFields(index, 1);
+                    });
+                    statsDisplayEmbed.setTitle('Overall Stats')
+                    statsDisplayEmbed.setDescription(convertArrayToString(trimmedArrayOfRaiders))
+
+                    initialMessage.edit({ embeds: [statsDisplayEmbed], components: [actionRow] }).catch(e => {})
+                });
+            } else {
+                // message.reply({ content: 'Grace period for the guild season ends <t:1692210540:R>. The raid stats for everyone(in PlayPal) will be reset <t:1692206940:R>. Message @bhaalu if you have any queries.', embeds: [statsDisplayEmbed] }).catch(err => console.log(err))
+                message.reply({ embeds: [statsDisplayEmbed] }).catch(err => console.log(err))
+            }
         })
 
     } catch (e) {
