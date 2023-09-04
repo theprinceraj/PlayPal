cab710418f4c871b18bf314ce4584c41c2e0c6be
Prince Raj <profile.princeraj+github@gmail.com>
2023-09-04 22:14:32 +0530

    Update path to use node:path

--- commands/Uncategorized/endguildseason.js
+++ commands/Uncategorized/endguildseason.js
@@ -1,9 +1,10 @@
 const firebase = require('../../firebase.js');
 const fs = require('fs');
+const path = require('node:path');
 const { AttachmentBuilder } = require('discord.js')
 
 exports.run = async (client, message, args) => {
-    if([!'564327207133249536'].includes(message.author.id)) return;
+    if ([!'564327207133249536'].includes(message.author.id)) return;
 
     let array = [];
 
@@ -22,7 +23,7 @@ exports.run = async (client, message, args) => {
 
         fs.writeFile(`storeroom/Guild_Stats_September_1st_Season.txt`, finalFileText, 'utf8', err => { })
 
-        const att = new AttachmentBuilder('storeroom/Guild_Stats_September_1st_Season.txt', 'Guild_Stats_September_1st_Season.txt')
+        const att = new AttachmentBuilder(path.join('storeroom' + 'Guild_Stats_September_1st_Season.txt'), 'Guild_Stats_September_1st_Season.txt')
         message.reply({ content: `Here is the text file the great respected honourable esteemed distinguished courteous revered gracious eminent polite regal admirable sir.`, files: [att] }).catch(error => console.error(error));
 
     } catch (error) {
