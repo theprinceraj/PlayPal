c7b76a179a4eee8a8d54ab666b3805bfbf6e4cde
Prince Raj <profile.princeraj+github@gmail.com>
2023-10-06 23:41:24 +0530

    Resets the raid data and saves it to a text file.

--- commands/Uncategorized/resetraidsdata.js
+++ commands/Uncategorized/resetraidsdata.js
@@ -10,12 +10,24 @@ exports.run = async (client, message, args) => {
             return;
         }
 
+        let array = [];
         const snapshot = await firebase.database().ref('raiders').once('value');
+        snapshot.forEach(childSnapshot => {
+            const raiderKey = childSnapshot.key;
+            const raiderData = childSnapshot.val();
+
+            array.push(`Raider ID: ${raiderKey}\nElixir Gained:${raiderData.elixirGained}\nRaids Lost: ${raiderData.raids.lost}\nRaids Won: ${raiderData.raids.won}\nTotal Raids: ${raiderData.raids.total}\nTotal XP: ${raiderData.xp.total}\nHighest XP: ${raiderData.xp.highest}\nLowest XP: ${raiderData.xp.lowest}\nMost Recent Raid XP: ${raiderData.xp.lastRaid}`);
+
+        });
+        array.unshift(`Guild Stats - October First Season`);
+        const finalFileText = array.join(`\n\n===========================================\n\n`)
+
+        fs.writeFile(`storeroom/Guild_Stats_October_1st_Season.txt`, finalFileText, 'utf8', err => { })
+
+
         snapshot.ref.remove();
 
-
         console.log(`====================\nRAID DATA WAS RESET BY ${message.author.username}\n====================`);
-
         message.reply('Raid data has been reset!').catch(console.error);
     } catch { }
 }
