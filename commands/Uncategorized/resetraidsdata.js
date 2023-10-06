dda9c3d14692c717c4c484854c8d8013e647bbb2
Prince Raj <profile.princeraj+github@gmail.com>
2023-10-06 23:23:07 +0530

    .

--- commands/Uncategorized/resetraidsdata.js
+++ commands/Uncategorized/resetraidsdata.js
@@ -4,18 +4,20 @@ const path = require('node:path');
 const { AttachmentBuilder } = require('discord.js')
 
 exports.run = async (client, message, args) => {
-    if (!['564327207133249536', '710735290993737759'].includes(message.author.id)) {
-        message.reply(`Only highly qualified royal people can use this command. ||just kidding, don't mind ;)||`).catch(console.error);
-        return;
-    }
+    try {
+        if (!['564327207133249536', '710735290993737759'].includes(message.author.id)) {
+            message.reply(`Only highly qualified royal people can use this command. ||just kidding, don't mind ;)||`).catch(console.error);
+            return;
+        }
 
-    const snapshot = await firebase.database().ref('raiders').once('value');
-    snapshot.ref.remove();
+        const snapshot = await firebase.database().ref('raiders').once('value');
+        snapshot.ref.remove();
 
 
-    console.log(`====================\nRAID DATA WAS RESET BY ${message.author.username}\n====================`);
+        console.log(`====================\nRAID DATA WAS RESET BY ${message.author.username}\n====================`);
 
-    message.reply('Raid data has been reset!').catch(console.error);
+        message.reply('Raid data has been reset!').catch(console.error);
+    } catch { }
 }
 
 exports.name = 'resetraidsdata'
