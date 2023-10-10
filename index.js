e2aefc3be916414c11f9473f50e07440e8f5d744
Prince Raj <profile.princeraj+github@gmail.com>
2023-10-10 08:08:57 +0530

    The diff removes the check for the specific user ID '853629533855809596' and replaces it with a check for two user IDs '853629533855809596' and '235148962103951360'.

--- index.js
+++ index.js
@@ -80,8 +80,8 @@ client.on('messageCreate', tradeRequestMessage => {
 // Using firebase
 const firebase = require('./firebase.js');
 client.on('messageCreate', message => {
-	if (message.author.id !== '853629533855809596') return;
-	// if (!['853629533855809596', '235148962103951360'].includes(message.author.id)) return;
+	// if (message.author.id !== '853629533855809596') return;
+	if (!['853629533855809596', '235148962103951360'].includes(message.author.id)) return;
 	const descriptionVar = message.embeds[0]?.description;
 
 	if (!descriptionVar || descriptionVar?.includes('mock raid')) return;
