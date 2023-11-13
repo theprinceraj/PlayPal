30e3155b7a84b6acd016377967934c91de602997
Prince Raj <profile.princeraj+github@gmail.com>
2023-11-13 16:06:03 +0530

    Update code to use EmbedBuilder, StringSelectMenuOptionBuilder, StringSelectMenuBuilder, and ActionRowBuilder classes

--- commands/Others/guildstats.js
+++ commands/Others/guildstats.js
@@ -1,5 +1,4 @@
-const { EmbedBuilder, StringSelectMenuOptionBuilder, StringSelectMenuBuilder, ActionRowBuilder, AttachmentBuilder } = require('discord.js');
-const path = require('node:path');
+const { EmbedBuilder, StringSelectMenuOptionBuilder, StringSelectMenuBuilder, ActionRowBuilder } = require('discord.js');
 const firebase = require('../../firebase.js');
 
 function convertArrayToString(arr) {
@@ -41,12 +40,8 @@ exports.run = (client, message, args) => {
         raidersRef.child(raiderSearched.id).once('value', async (snapshot) => {
             const raiderData = snapshot.val();
 
-            // const att = new AttachmentBuilder(path.join('storeroom', 'Guild_Stats_September_2nd_Season_copy.txt'), 'Guild_Stats_September_2nd_Season_copy.txt')
-
             if (!raiderData) {
                 message.reply('No stat found, do a raid with `sgr` and then try again.').catch(err => console.log(err));
-                // message.reply({ content: `Here is the raid data for the previous season. It won't be sent after 24 hours so make sure that you save it right now!`, files: [att] }).catch(error => console.error(error));
-
                 return;
             }
 
@@ -65,64 +60,9 @@ exports.run = (client, message, args) => {
                     { name: '<a:rocket_emoji:1141006841132879872> Join my support server to report issues or bugs or downtimes', value: `[Click here](https://discord.gg/SxwEffMWch)` }
                 )
 
-            if (message.author.id === '564327207133249536') {
-                // Make a select menu
-                const select = new StringSelectMenuBuilder()
-                    .setCustomId('guild-stats-dropdown')
-                    .setPlaceholder('Make a selection!')
-                    .addOptions(
-                        new StringSelectMenuOptionBuilder()
-                            .setLabel('Ascending Order')
-                            .setDescription('Sorts people in ascending order of their XP.')
-                            .setValue('ascending')
-                            .setEmoji('↗️'),
-                        new StringSelectMenuOptionBuilder()
-                            .setLabel('Descending Order')
-                            .setDescription('Sorts people in ascending order of their XP.')
-                            .setValue('descending')
-                            .setEmoji('↘️'),
-                    );
-                const actionRow = new ActionRowBuilder()
-                    .addComponents(select)
+            message.reply({ content: 'I just made a support server now for reporting such issues, join if you want to!', embeds: [statsDisplayEmbed] }).catch(err => console.log(err));
 
-                let initialMessage = await message.reply({ content: '', embeds: [statsDisplayEmbed], components: [actionRow] }).catch(err => console.log(err))
-
-
-                // message.reply({ content: `Here is the raid data for the previous season. It won't be sent after 24 hours so make sure that you save it right now!`, files: [att] }).catch(error => console.error(error));
-
-                client.on('interactionCreate', async (interaction) => {
-                    if (!interaction.isStringSelectMenu() || interaction.customId !== 'guild-stats-dropdown') return;
-                    let arrayOfRaiders = [];
-                    const raidersDatabase = await raidersRef.once('value')
-                    raidersDatabase.forEach(raiderEntry => {
-                        let raiderId = raiderEntry.key;
-                        let raiderDetails = raiderEntry.val();
-                        arrayOfRaiders.push({ raiderId: raiderId, totalXp: raiderDetails.xp.total })
-                    })
-                    let sortedArray = [];
-                    if (interaction.values[0] === 'ascending') {
-                        sortedArray = arrayOfRaiders.sort((a, b) => a.totalXp - b.totalXp)
-                    } else if (interaction.values[0] === 'descending') {
-                        sortedArray = arrayOfRaiders.sort((a, b) => b.totalXp - a.totalXp)
-                    }
-
-                    let trimmedArrayOfRaiders = sortedArray.slice(0, 6);
-
-                    // Remove fields from the embed
-                    [0, 1, 2].forEach(index => {
-                        statsDisplayEmbed.spliceFields(index, 1);
-                    });
-                    statsDisplayEmbed.setTitle('Overall Stats')
-                    statsDisplayEmbed.setDescription(convertArrayToString(trimmedArrayOfRaiders))
-
-                    initialMessage.edit({ embeds: [statsDisplayEmbed], components: [actionRow] }).catch(e => { })
-                });
-            } else {
-
-                message.reply({ content: 'I just made a support server now for reporting such issues, join if you want to!', embeds: [statsDisplayEmbed] }).catch(err => console.log(err));
-
-                // message.reply({ content: `Here is the raid data for the previous season. It won't be sent after 24 hours so make sure that you save it right now!`, files: [att] }).catch(error => console.error(error));
-            }
+            // message.reply({ content: `Here is the raid data for the previous season. It won't be sent after 24 hours so make sure that you save it right now!`, files: [att] }).catch(error => console.error(error));
         })
 
     } catch (e) {
