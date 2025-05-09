21a63fa6e55e1ddd8557636817c13a9f048c1cc0
Prince Raj <profile.princeraj@gmail.com>
2025-05-09 23:48:46 +0530

    .

--- firebase.js
+++ firebase.js
@@ -1,16 +1,16 @@
-const config = require('./config.json');
+const config = require("./config.json");
 
-const firebase = require('firebase');
+const firebase = require("firebase/app");
 
 const firebaseConfig = {
-	apiKey: config.FIREBASE.apiKey,
-	authDomain: config.FIREBASE.authDomain,
-	databaseURL: config.FIREBASE.databaseURL,
-	projectId: config.FIREBASE.projectId,
-	storageBucket: config.FIREBASE.storageBucket,
-	messagingSenderId: config.FIREBASE.messagingSenderId,
-	appId: config.FIREBASE.appId,
-	measurementId: config.FIREBASE.measurementId
+    apiKey: config.FIREBASE.apiKey,
+    authDomain: config.FIREBASE.authDomain,
+    databaseURL: config.FIREBASE.databaseURL,
+    projectId: config.FIREBASE.projectId,
+    storageBucket: config.FIREBASE.storageBucket,
+    messagingSenderId: config.FIREBASE.messagingSenderId,
+    appId: config.FIREBASE.appId,
+    measurementId: config.FIREBASE.measurementId,
 };
 firebase.initializeApp(firebaseConfig);
 
